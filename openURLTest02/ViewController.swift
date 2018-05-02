98c4564c1cc4849841d917e74e48a078684a172b
Masahiko Minagawa <Masahiko.Minagawa@jp.sony.com>
2018-05-02 19:40:14 +0900

    Initial Commit

---
+++ openURLTest02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  openURLTest02
+//
+//  Created by Masahiko Minagawa on 2018/05/02.
+//  Copyright © 2018年 Sony Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
